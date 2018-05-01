23153ef07b7cd7258c2fa7c059c789339b1eae74
Joel Smith <joelsmith@Joels-MacBook-Pro.local>
2018-05-01 14:20:58 -0400

    created folders for actions, components, containers and reducers

--- src/index.js
+++ src/index.js
@@ -2,32 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import {Router, Route, browserHistory } from 'react-router';
-import LogIn from './loginScreen';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import reducers from 'reducers';
-import { syncHistoryWithStore } from 'react-router-redux'
-import thunk from 'redux-thunk'
 
-const store = createStore(reducers, composeWithDevTools(
-    applyMiddleware(thunk)
-  ))
-
-const history = syncHistoryWithStore(browserHistory, store)
-
-ReactDOM.render(
-    // <App />
-    <Provider store={store} >
-        <Router history={history} >
-            {/* <Route component={Layout} /> */} */}
-                <Route path='/' component={LogIn} />
-                <Route path='/users/:userId' />
-        </Router>
-    </Provider>,
-     document.getElementById('root')
+ReactDOM.render(<App />, document.getElementById('root')
 );
 
-registerServiceWorker();
+
